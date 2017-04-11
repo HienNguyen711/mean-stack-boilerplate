57c07ef241690b1076539abaf365ebb4d9a58e05
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-11 23:23:25 +0300

    update mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const config = require('./config');
+const mongoose = require('mongoose');
+
+module.exports = function() {
+  const db = mongoose.connect(config.db);//connect to mongodb
+
+  return db;
+};
