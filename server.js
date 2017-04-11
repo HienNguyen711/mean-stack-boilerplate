ac1a7491f6714049edd1197f5f7b1c692eeacf5b
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-11 23:24:01 +0300

    update server

--- server.js
+++ server.js
@@ -54,3 +54,16 @@ module.exports = app;
 // app.get('/', (req, res) => {
 //   res.status(200).send('This is a GET request');
 // });
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const configureMongoose = require('./config/mongoose');
+const configureExpress = require('./config/express');
+
+const db = configureMongoose();
+const app = configureExpress();
+app.listen(3000);
+
+module.exports = app;
+console.log('Server running at http://localhost:3000/');
