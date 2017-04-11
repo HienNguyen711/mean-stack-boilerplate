841d8c37880616c11d9c1942727246269c7ab1e4
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-11 23:10:46 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 'use strict';
 
-const http = require('http');
+// const http = require('http');
 
-http.createServer(function(req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  });
+// http.createServer(function(req, res) => {
+//   res.writeHead(200, {
+//     'Content-Type': 'text/plain'
+//   });
+//   res.end('Hello World');
+// //   res.write('Hello hien');
+// //   res.end();
+// }).listen(3000);
+
+
+// console.log('Server running at http://localhost:3000/');
+
+
+const connect = require('connect');
+const app = connect();
+
+function logger(req, res, next) {
+  console.log(req.method, req.url);
+  next();
+};
+
+
+function helloWorld(req, res, next) {
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
-//   res.write('Hello hien');
-//   res.end();
-}).listen(3000);
+};
+app.use(logger);
+app.use('/helloworld',helloWorld);
 
-
+app.listen(3000); 
 console.log('Server running at http://localhost:3000/');
