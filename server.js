ac208bd81e8b4cf060836e204576484383a6cbc4
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-11 23:25:09 +0300

    update server

--- server.js
+++ server.js
@@ -1,40 +1,13 @@
 'use strict';
 
-// const http = require('http');
 
-// http.createServer(function(req, res) => {
-//   res.writeHead(200, {
-//     'Content-Type': 'text/plain'
-//   });
-//   res.end('Hello World');
-// //   res.write('Hello hien');
-// //   res.end();
-// }).listen(3000);
-
-
-// console.log('Server running at http://localhost:3000/');
-
-
-// const connect = require('connect');
-// const app = connect();
-
-// function logger(req, res, next) {
-//   console.log(req.method, req.url);
-//   next();
-// };
-
-
-// function helloWorld(req, res, next) {
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World');
-// };
-// app.use(logger);
-// app.use('/helloworld',helloWorld);
-
-// app.listen(3000); 
-// console.log('Server running at http://localhost:3000/');
 const express = require('express');
 const app = express();
+
+
+
+const PORT = process.env.PORT || 3000;
+const ENV = process.env.NODE_ENV || 'development';
 
 app.use('/', (req, res) => {
   res.status(200).send('Hello World');
@@ -56,7 +29,7 @@ module.exports = app;
 // });
 
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 
 const configureMongoose = require('./config/mongoose');
 const configureExpress = require('./config/express');
