72dfcaa00b6289a31d6ad985b0e21466af37ec66
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-11 23:13:57 +0300

    add file structure

--- server.js
+++ server.js
@@ -15,21 +15,32 @@
 // console.log('Server running at http://localhost:3000/');
 
 
-const connect = require('connect');
-const app = connect();
+// const connect = require('connect');
+// const app = connect();
 
-function logger(req, res, next) {
-  console.log(req.method, req.url);
-  next();
-};
+// function logger(req, res, next) {
+//   console.log(req.method, req.url);
+//   next();
+// };
 
 
-function helloWorld(req, res, next) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-};
-app.use(logger);
-app.use('/helloworld',helloWorld);
+// function helloWorld(req, res, next) {
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// };
+// app.use(logger);
+// app.use('/helloworld',helloWorld);
 
-app.listen(3000); 
+// app.listen(3000); 
+// console.log('Server running at http://localhost:3000/');
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => {
+  res.status(200).send('Hello World');
+});
+
+app.listen(3000);
 console.log('Server running at http://localhost:3000/');
+
+module.exports = app;
