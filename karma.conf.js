22808c83279f4c698c83ea6806eae94cf1724b77
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-12 10:08:09 +0300

    edit karma config file

--- karma.conf.js
+++ karma.conf.js
@@ -10,12 +10,35 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'requirejs'],
+    frameworks: ['jasmine'],
 
 
     // list of files / patterns to load in the browser
     files: [
+      'node_modules/systemjs/dist/system.js',
+      'node_modules/systemjs/dist/system-polyfills.js',
+      'node_modules/core-js/client/shim.min.js',
+      'node_modules/reflect-metadata/Reflect.js',
+      'node_modules/zone.js/dist/zone.js',
+      'node_modules/zone.js/dist/long-stack-trace-zone.js',
+      'node_modules/zone.js/dist/proxy.js',
+      'node_modules/zone.js/dist/sync-test.js',
+      'node_modules/zone.js/dist/jasmine-patch.js',
+      'node_modules/zone.js/dist/async-test.js',
+      'node_modules/zone.js/dist/fake-async-test.js',
+
+      { pattern: 'public/systemjs.config.js', served: true,included: false, watched: false },
+      { pattern: 'public/app/**/*.*', served: true, included:false, watched: false },
+      { pattern: 'node_modules/rxjs/**/*.js', served: true,included: false, watched: false },
+      { pattern: 'node_modules/@angular/**/*.js', served:true,included: false, watched: false },
+
+      'karma.shim.js'
     ],
+
+    proxies: {
+      '/lib/': '/base/node_modules/',
+      '/app/': '/base/public/app/',
+    },
 
 
     // list of files to exclude
@@ -54,12 +77,13 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
+    captureTimeout: 60000,
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
