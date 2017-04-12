1cf79c38ff0bf32987b2be1cd5f49b9eea75dae1
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-12 09:54:13 +0300

    add testing for the model in back end

--- config/environments/test.js
+++ config/environments/test.js
@@ -1,1 +1,22 @@
-'use strict';
+'use strict'
+module.exports = {
+  db: 'mongodb://localhost/mean-stack-boilerplate-test',
+  sessionSecret: 'Your Application Session Secret',
+  viewEngine: 'ejs',
+  facebook: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+  },
+  twitter: 
+  {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/oauth/twitter/callback'
+  },
+  google: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/oauth/google/callback'
+  }
+};
