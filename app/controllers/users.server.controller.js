b74b92398df2ad4b2c512aec6f270ce17145e3d5
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-15 19:49:38 +0300

    create user controller

---
+++ app/controllers/users.server.controller.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+const User = require('mongoose').model('User');
+
+exports.create = function(req, res, next) {
+  const user = new User(req.body);
+
+  user.save((err) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.status(200).json(user);
+    }
+  });
+};
