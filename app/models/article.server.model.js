6fd037a150ceb2e7b6eb19d1f00d36f394f7bf05
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-16 22:46:13 +0300

    start with article in the back end

--- app/models/article.server.model.js
+++ app/models/article.server.model.js
@@ -3,5 +3,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    title: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Title cannot be blank'
+    },
+    content: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    location: {
+        type: String,
+        trim:true
+    },
+    creator: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
 
 });
+
+mongoose.model('Article', ArticleSchema);
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,12 @@
 'use strict';
-
 const config = require('./config');
 const mongoose = require('mongoose');
 
 module.exports = function() {
-  const db = mongoose.connect(config.db);//connect to mongodb
+  const db = mongoose.connect(config.db);//connect to mongodb 
+
+  require('../app/models/user.server.model');
+  require('../app/models/article.server.model');
 
   return db;
 };
