79a876eef5e8d1be42079f25f1860c28df57a0ee
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-16 22:49:41 +0300

    error with mongoose in the back end

--- app/controllers/article.server.controller.js
+++ app/controllers/article.server.controller.js
@@ -1,1 +1,18 @@
 'use strict';
+const mongoose = require('mongoose');
+const Article = mongoose.model('Article');//article model
+
+//handle error in mongoose
+function getErrorMessage (err) {
+    if(err.errors){
+        for(let errName in err.errors){
+            if(err.errors[errName].message) {
+                return err.errors[errName].message;
+            } else {
+                return 'Unknown server error';
+            }
+        }
+    }
+
+
+};
